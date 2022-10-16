502549c62986b0022009874d55f1db23e943d456
damolabolaji <bolaji.oyindamola@gmail.com>
2022-10-16 12:44:20 +0100

    exercise done

--- review-summer-games/js/main.js
+++ review-summer-games/js/main.js
@@ -1,1 +1,13 @@
-//Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+//Create a function that takes in an array of numbers. 
+//Multiply each number together and alert the product.
+
+function multiplyEach(array){
+    sum = 1
+    array.forEach(item => {
+        sum = sum * item
+        
+    })
+    alert(sum)
+}
+
+multiplyEach([1,2,3,4,5])
