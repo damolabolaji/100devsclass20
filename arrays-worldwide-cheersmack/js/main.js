f2611247345f43a453248dce1d1b1760a4394224
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-10-20 18:46:04 +0100

    Update main.js
    solved the exercise

--- arrays-worldwide-cheersmack/js/main.js
+++ arrays-worldwide-cheersmack/js/main.js
@@ -1,1 +1,16 @@
-//Create a function that takes in a number. The function should return an array that contains every number from 1 to that number as seperate elements
+
+
+//Create a function that takes in a number. The function 
+//should return an array that contains every number 
+//from 1 to that number as seperate elements
+
+function listNum(num){
+    let numArray = []
+    for(let i = 1; i <= num; i++){
+        numArray.push(i)
+    }
+
+    console.log(numArray)
+}
+
+listNum(50)
